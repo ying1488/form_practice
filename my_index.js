326c22d4c52bb060181b0d4b36692e96afbb20e9
ying1488 <yingecteach@gmail.com>
2023-07-28 21:39:15 +1000

    tidy up CSS with Bulma

--- my_index.js
+++ my_index.js
@@ -56,43 +56,24 @@ function onSubmit(e) {
 
 
     // style div with colours 
-
-
-
     const makeRandColor = () => {
         const r = Math.floor(Math.random() * 255);
         const g = Math.floor(Math.random() * 255);
         const b = Math.floor(Math.random() * 255);
         return `rgb(${r},${g},${b})`;
     }
-    const newDiv = newLI.innerText;
+    const newDiv = newLI;
+    //newDiv 
     const newColor = makeRandColor();
-    document.body.div.style.div.background = newColor;
-    newDiv.newColor
+    newDiv.style.backgroundColor = newColor;
+
 
 
 }
 
-
 form.addEventListener("submit", onSubmit);
 
 
-
-// const button = document.querySelector('button');
-// const h1 = document.querySelector('h1');
-
-// button.addEventListener('click', function () {
-//     const newColor = makeRandColor();
-//     document.body.style.backgroundColor = newColor;
-//     h1.innerText = newColor;
-// })
-
-// const makeRandColor = () => {
-//     const r = Math.floor(Math.random() * 255);
-//     const g = Math.floor(Math.random() * 255);
-//     const b = Math.floor(Math.random() * 255);
-//     return `rgb(${r},${g},${b})`;
-// }
 
 
 
